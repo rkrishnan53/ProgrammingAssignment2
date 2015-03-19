e8284fe6d2c33c82607388d2916a556037260b4d
rkrishnan53 <rkrishman@comcast.net>
2015-03-18 17:31:51 -0700

    RKrishnan's cachematrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##makeCacheMatrix: Create a cache space in the global environment to hold a matrix and it's inverse. Preset both with NULL.makeCacheMatrix returns a list which includes two intermediate objects set and get but also the original matrix and it's inverse
+##cacheSolve: Checks if the inverse exists (i.e. non-NULL). 
+##If NULL, it computes the inverse using solve and caches it for future iterations.
+##If non-NULL, cacheSolve retrieves the inverse from the cache
+##
+makeCacheMatrix<-function(x=matrix()){
+	m<-NULL					##preset m (inverse) to NULL
+	set<-function(y){		##define function that... 
+		x<<-y				##caches orig matrix ....
+		m<<-NULL			##and the inverse
+	}	
+	get<-function() x		##define function that creates a list...
+	setmatrix<-function(solve) m<<-solve	##with the cached matrix and inverse
+	getmatrix<-function() m
+	list(set=set,get=get,setmatrix=setmatrix,getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x=matrix(),...){	##create function to...
+	m<-x$getmatrix()					
+	if(!is.null(m)){					##get inverse from cache IF NON-NULL
+		message("getting cached data")
+		return(m)						
+			}
+	matrix<-x$get()						##or, if Inverse NULL (first time)
+	m<-solve(matrix,...)				##...then compute inverse using solve()
+	x$setmatrix(m)						
+	m									##..and return inverse
 }
